979dcb97f6af97218c9144ce32800216efca83ad
mcsreddy555 <chandumullangi3@gmail.com>
2020-12-29 16:44:57 +0530

    chat-angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ChatBoxComponent } from './component/chat-box/chat-box.component';
+import { NotificationsComponent } from './component/notifications/notifications.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChatBoxComponent,
+    NotificationsComponent
   ],
   imports: [
     BrowserModule
